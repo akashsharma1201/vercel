9699604fb8fab5846151d9e605f9173074d40c68
Vikashsharma2003 <vikashsharma20103@gmail.com>
2023-04-21 13:30:58 +0530

    add post request

--- server.js
+++ server.js
@@ -6,6 +6,11 @@ app.get("/" , (req ,res)=>{
     res.send("vercel node upload")
 })
 
+app.post("/register" , (req ,res)=>{
+    res.send("post request",req.body)
+})
+
+
 app.listen("5000" , ()=>{
     console.log("running on 5000 port");
 })
