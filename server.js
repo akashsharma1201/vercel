bfb3c20d0acf502f276974797104a07faac2c2e2
Vikashsharma2003 <vikashsharma20103@gmail.com>
2023-04-21 17:19:26 +0530

    2

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const app = express();
 
 
 // app.use(express.urlencoded({ extended: true }));
-app.use(express.json())
+// app.use(express.json())
 app.get("/" , (req ,res)=>{
     res.send("vercel node upload")
 })
